bc9b292649fdc0f43270b3192d276eed17e36de6
theohk <theo.okeno@gmail.com>
2024-04-26 05:28:19 +0530

    home page card

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,23 +6,31 @@ import ListHomeSongs from "@/features/home/ListHomeSongs";
 function Home() {
   return (
     <>
-    <div className="flex flex-col gap-4 justify-center items-center mx-auto mb-12">
-      <div className="pl-6 xl:pl-2 self-start text-2xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-blue-400 to-red-600">
-        Trending Artists
+      <div className="w-[90%] md:w-[70%] bg-neutral-100 h-64 shadow-md flex flex-col items-center justify-center mx-auto mb-12 rounded-xl ">
+        <div className="text-2xl font-extralight">WELCOME TO</div>
+        <div className="text-6xl font-semibold text-transparent bg-clip-text bg-gradient-to-br from-green-400 to-stone-500 mb-6">
+          ZOAWI
+        </div>
+        <div className="text-xl font-extralight">Lowkey the hippest lyrics</div>
+        <div className="text-xl font-extralight">database in Mizoram. No cap.</div>
       </div>
-      <div className="">
-        <CarouselHomeArtists />
+      <div className="flex flex-col gap-4 justify-center items-center mx-auto mb-12">
+        <div className="pl-6 xl:pl-2 self-start text-2xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-blue-400 to-red-600">
+          Trending Artists
+        </div>
+        <div className="">
+          <CarouselHomeArtists />
+        </div>
       </div>
-    </div>
 
-    <div className="flex flex-col gap-4 justify-center items-center mx-auto">
-      <div className="pl-6 xl:pl-2 self-start text-2xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-red-400 to-gray-400">
-        Hot Songs
+      <div className="flex flex-col gap-4 justify-center items-center mx-auto">
+        <div className="pl-6 xl:pl-2 self-start text-2xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-red-400 to-gray-400">
+          Hot Songs
+        </div>
+        <div className="">
+          <ListHomeSongs />
+        </div>
       </div>
-      <div className="">
-        <ListHomeSongs />
-      </div>
-    </div>
     </>
   );
 }
