e97ba2a415fc6ee0f1d40cdfd045ff8e792324bc
theohk <theo.okeno@gmail.com>
2024-05-22 16:25:04 +0530

    added lazy load component

--- src/features/artists/ArtistCard.jsx
+++ src/features/artists/ArtistCard.jsx
@@ -1,3 +1,5 @@
+import { LazyLoadImage } from "react-lazy-load-image-component";
+
 function ArtistCard({ children, imgSrc, ...props }) {
   return (
     <div
@@ -5,16 +7,25 @@ function ArtistCard({ children, imgSrc, ...props }) {
       className=" cursor-pointer relative h-52 sm:h-60 overflow-hidden rounded-2xl shadow-lg group border-2 border-secondary border-dashed"
     >
       {imgSrc ? (
-          <img
+        <>
+          {/* <img
             src={imgSrc}
+            loading="lazy"
             alt="/"
             className="transition-transform group-hover:scale-110 duration-200 object-cover w-full h-full"
-          />
+          /> */}
+
+          <LazyLoadImage
+            className="transition-transform group-hover:scale-110 duration-200 object-cover w-full h-full"
+            alt="/"
+            src={imgSrc}
+            />
+        </>
       ) : (
         <div
           className="transition-transform group-hover:scale-110 duration-200 bg-gray-200  object-cover h-full w-full"
           style={{ width: "100%", paddingTop: "100%" }}
-        ></div>
+        >fff</div>
       )}
       <div className="absolute inset-0 flex items-end bg-gradient-to-t from-black via-transparent to-transparent">
         <div className="p-4 uppercase text-white/70">{children}</div>
